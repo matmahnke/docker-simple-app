53db17f238d5a5af5d4dba0cca5a4ca7b245bbfd
matheus mahnke <matheusmahnke@gmail.com>
2019-09-23 18:02:11 -0300

    commit inicial

--- PeerOne/Controllers/ValuesController.cs
+++ PeerOne/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace PeerOne.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "peerone1" };
         }
 
         // GET api/values/5
